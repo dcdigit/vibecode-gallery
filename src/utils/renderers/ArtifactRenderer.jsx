163b1f771cad15e3194585495266e8a8f65f28ac
dc <dcd.bbs+netlify@gmail.com>
2025-03-12 23:40:37 -0400

    Squashed commit of the following:
    commit c829f4aa22e472fb99150a964023f18191d9bf70
    Author: dc <dcd.bbs+netlify@gmail.com>
    Date:   Wed Mar 12 23:38:48 2025 -0400
    
        remove dates since they are stat mod time not relevant to static build sites
    
    commit 4bb8ffbcb50af03c258dc161e4b9a7152a8c2bad
    Author: dc <dcd.bbs+netlify@gmail.com>
    Date:   Wed Mar 12 23:35:23 2025 -0400
    
        working
    
    commit b003f38aa1dbd6a4ac2ae59b17b523d0a4b70862
    Author: dc <dcd.bbs+netlify@gmail.com>
    Date:   Wed Mar 12 23:02:00 2025 -0400
    
        cleanup
    
    commit c49e2fa91d552553e8bfdd340b84afd7db72dce3
    Author: dc <dcd.bbs+netlify@gmail.com>
    Date:   Wed Mar 12 22:52:19 2025 -0400
    
        live pre-rendered components/artifacts in index

--- src/utils/renderers/ArtifactRenderer.jsx
+++ src/utils/renderers/ArtifactRenderer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import artifactComponents from './artifactComponents';
 
 /**
@@ -8,6 +8,16 @@ import artifactComponents from './artifactComponents';
  * @param props.props - Props to pass to the rendered component
  */
 export default function ArtifactRenderer({ componentName, props = {} }) {
+  const [renderAttempted, setRenderAttempted] = useState(false);
+
+  useEffect(() => {
+    setRenderAttempted(true);
+    
+    return () => {
+      // Cleanup if needed
+    };
+  }, [componentName, props]);
+
   // Get the component from the components map
   const Component = artifactComponents[componentName];
   
@@ -17,10 +27,21 @@ export default function ArtifactRenderer({ componentName, props = {} }) {
       <div className="p-4 border-2 border-red-500 rounded bg-red-100 text-red-800">
         <h2 className="text-xl font-bold mb-2">Component Not Found</h2>
         <p>The component "{componentName}" does not exist in the artifacts collection.</p>
+        <p className="mt-2 text-sm">Available components: {Object.keys(artifactComponents).join(', ')}</p>
       </div>
     );
   }
   
   // Render the component with the provided props
-  return <Component {...props} />;
+  try {
+    return <Component {...props} />;
+  } catch (error) {
+    return (
+      <div className="p-4 border-2 border-red-500 rounded bg-red-100 text-red-800">
+        <h2 className="text-xl font-bold mb-2">Rendering Error</h2>
+        <p>Failed to render component "{componentName}"</p>
+        <p className="mt-2 text-sm">Error: {error.message}</p>
+      </div>
+    );
+  }
 }
